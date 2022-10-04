ae7a15c44959e6d553706d1206329e8aa6565c33
Gustavo <gustavo.felipe01@outlook.com>
2022-10-04 08:56:39 -0300

    build

--- src/scripts/app/index.js
+++ src/scripts/app/index.js
@@ -1,5 +1,10 @@
 import Allin from './modules/allin'
 import ModalZap from './modules/modalZap'
+import Home from './pages/home'
+
+const $bodyClass = document.querySelector('body');
+console.log($bodyClass.className.toLowerCase().includes('internasComum hotsitev2 colecao everbrew'))
 
 ModalZap()
 Allin()
+$bodyClass.className.toLowerCase().includes('home') && Home()
