484e2128774205441fba193b9ed9247c2364fd9e
Gustavo <gustavo.felipe01@outlook.com>
2022-10-05 08:46:08 -0300

    adjust in modal zap

--- src/scripts/app/index.js
+++ src/scripts/app/index.js
@@ -1,10 +1,9 @@
-import Allin from './modules/allin'
 import ModalZap from './modules/modalZap'
 import Home from './pages/home'
 
 const $bodyClass = document.querySelector('body');
 console.log($bodyClass.className.toLowerCase().includes('internasComum hotsitev2 colecao everbrew'))
 
-ModalZap()
-Allin()
+
+!$bodyClass.className.toLowerCase().includes('clube-de-cervejas') && ModalZap()
 $bodyClass.className.toLowerCase().includes('home') && Home()
