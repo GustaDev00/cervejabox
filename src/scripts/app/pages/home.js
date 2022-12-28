adc5340a5a1795f1fad91374b8fef293f82b737a
Gustavo Pontes <gustavo.pontes@foreverliss.com.br>
2022-12-28 09:55:26 -0300

    feat: add schema

--- src/scripts/app/pages/home.js
+++ src/scripts/app/pages/home.js
@@ -1,8 +1,50 @@
-import WapperLinkBanner from '../modules/wapperLinkBanner'
+import WapperLinkBanner from "../modules/wapperLinkBanner";
 
 const Home = () => {
-    WapperLinkBanner();
-}
+  WapperLinkBanner();
 
+  const head = document.getElementsByTagName("head")[0];
+  const script = document.createElement("script");
+  script.type = "application/ld+json";
 
-export default Home
+  var jsonLD = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    headline: "CervejaBox | Maior Loja Online pra Comprar Cerveja Artesanal!",
+    url: "https://www.cervejabox.com.br/",
+    about: [
+      {
+        "@type": "Thing",
+        name: "Cerveja Artesanal",
+        sameAs: "https://pt.wikipedia.org/wiki/Cerveja",
+      },
+      {
+        "@type": "Thing",
+        name: "Cervejarias",
+        sameAs: "https://pt.wikipedia.org/wiki/Cervejaria",
+      },
+      {
+        "@type": "Thing",
+        name: "Clube de Cerveja",
+        sameAs: "https://pt.wikipedia.org/wiki/Modelo_de_assinatura",
+      },
+    ],
+    mentions: [
+      {
+        "@type": "Brand",
+        name: "Cerveja Brewdog",
+        sameAs: "https://www.cervejabox.com.br/cervejarias/brewdog",
+      },
+      {
+        "@type": "Brand",
+        name: "Cervejas Bierland",
+        sameAs: "https://www.cervejabox.com.br/cervejarias/bierland",
+      },
+    ],
+  };
+
+  script.text = JSON.stringify(jsonLD);
+  head.appendChild(script);
+};
+
+export default Home;
